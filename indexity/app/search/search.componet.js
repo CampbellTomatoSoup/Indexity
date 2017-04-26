ea7fdaec8bf0667e12ba4868a80ee3ab728318cf
CampbellTomatoSoup <campbell.konrad@gmail.com>
2017-04-26 16:24:35 -0500

    drop down menu and cities listing

--- indexity/app/search/search.componet.js
+++ indexity/app/search/search.componet.js
@@ -5,6 +5,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
+$(".dropdown-menu li a").click(function(){
+  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+});
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 let SearchComponent = class SearchComponent {
