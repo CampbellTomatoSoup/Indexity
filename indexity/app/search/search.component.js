0bccd2b88ee372885e753dd94a659675ecb4d8d7
Campbell Konrad <campbell.konrad@gmail.com>
2017-05-02 22:22:05 -0500

    Update search.component.js

--- indexity/app/search/search.component.js
+++ indexity/app/search/search.component.js
@@ -10,9 +10,46 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
+const router_1 = require("@angular/router");
+const search_repository_1 = require("../api/search-repository");
 let SearchComponent = class SearchComponent {
-    constructor() { }
+    constructor(userService, router) {
+        this.userService = userService;
+        this.router = router;
+        //a list of the cities
+        this.cities = [
+            { value: 'austin', display: 'Austin' },
+            { value: 'baltimore', display: 'Baltimore' },
+            { value: 'boston', display: 'Boston' },
+            { value: 'columbus', display: 'Columbus' },
+            { value: 'dallas', display: 'Dallas' },
+            { value: 'denver', display: 'Denver' },
+            { value: 'detroit', display: 'Detroit' },
+            { value: 'honolulu', display: 'Honolulu' },
+            { value: 'houston', display: 'Houston' },
+            { value: 'lasvegas', display: 'Las Vegas' },
+            { value: 'losangeles', display: 'Los Angeles' },
+            { value: 'memphis', display: 'Memphis' },
+            { value: 'newyork', display: 'New York' },
+            { value: 'orlando', display: 'Orlando' },
+            { value: 'philidelphia', display: 'Philidelphia' },
+            { value: 'pittsburgh', display: 'Pittsburgh' },
+            { value: 'sanantonio', display: 'San Antonio' },
+            { value: 'sandiego', display: 'San Diego' },
+            { value: 'sanfransisco', display: 'San Fransisco' },
+            { value: 'seattle', display: 'Seattle' },
+            { value: 'stlouis', display: 'St. Louis' },
+            { value: 'dc', display: 'D.C.' },
+        ];
+    }
     ngOnInit() {
+        this.user = {
+            city1: null,
+            city2: null
+        };
+    }
+    save(isValid, f) {
+        console.log(f);
     }
 };
 SearchComponent = __decorate([
@@ -21,7 +58,7 @@ SearchComponent = __decorate([
         templateUrl: './app/search/search.component.html',
         styleUrls: ['./app/search/search.component.css']
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [search_repository_1.SearchRepository, router_1.Router])
 ], SearchComponent);
 exports.SearchComponent = SearchComponent;
 //# sourceMappingURL=search.component.js.map
