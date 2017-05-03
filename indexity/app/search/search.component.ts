2a3b02c33ea01bbcb67f08fcc74736ce2559fdad
Campbell Konrad <campbell.konrad@gmail.com>
2017-05-02 22:20:55 -0500

    Update search.component.ts

--- indexity/app/search/search.component.ts
+++ indexity/app/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../api/user.interface';
+import { Theme } from '../api/theme.interface';
+import { Router } from '@angular/router';
+import { SearchRepository } from '../api/search-repository';
 
 @Component({
   selector: 'search',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app/search/search.component.css']
 })
 export class SearchComponent implements OnInit {
+  constructor(private userService: SearchRepository, private router: Router) { }
 
-  constructor() { }
+  public user: User;
 
-  ngOnInit() {
+//selected costs
+  publicTrans: false;
+  housing: false;
+  utilities: false;
+  groceries: false;
+  healthcare: false;
+  incomeTax: false;
+  salesTax: false;
+
+//a list of the cities
+  public cities = [
+    { value: 'austin', display: 'Austin' },
+    { value: 'baltimore', display: 'Baltimore' },
+    { value: 'boston', display: 'Boston' },
+    { value: 'columbus', display: 'Columbus' },
+    { value: 'dallas', display: 'Dallas' },
+    { value: 'denver', display: 'Denver' },
+    { value: 'detroit', display: 'Detroit' },
+    { value: 'honolulu', display: 'Honolulu' },
+    { value: 'houston', display: 'Houston' },
+    { value: 'lasvegas', display: 'Las Vegas' },
+    { value: 'losangeles', display: 'Los Angeles' },
+    { value: 'memphis', display: 'Memphis' },
+    { value: 'newyork', display: 'New York' },
+    { value: 'orlando', display: 'Orlando' },
+    { value: 'philidelphia', display: 'Philidelphia' },
+    { value: 'pittsburgh', display: 'Pittsburgh' },
+    { value: 'sanantonio', display: 'San Antonio' },
+    { value: 'sandiego', display: 'San Diego' },
+    { value: 'sanfransisco', display: 'San Fransisco' },
+    { value: 'seattle', display: 'Seattle' },
+    { value: 'stlouis', display: 'St. Louis' },
+    { value: 'dc', display: 'D.C.' },
+];
+
+  ngOnInit(){
+    this.user = {
+    city1: null,
+    city2: null
   }
-
 }
+
+public save(isValid: boolean, f: User) {
+        console.log(f);
+    }
+  }
